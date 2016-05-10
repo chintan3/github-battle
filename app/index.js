58a1646fb436eb8319750070b2ff8a4548e0d4af
Chintakumar Prajapati <cprajapati@stratag.com>
2016-05-10 13:59:33 -0500

    hello world  working in react

--- app/index.js
+++ app/index.js
@@ -1,2 +1,12 @@
-var app = document.getElementById('app');
-app.innerHTML = 'hellO';
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+  render: function () {
+    return (
+      <div>Hello World!</div>
+    )
+  }
+});
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
